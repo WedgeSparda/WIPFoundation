049959325cb4357eb585141df3b45e56d29e3af5
Roberto Pastor Ortiz <roberto.pastor@cabify.com>
2021-05-03 15:53:48 +0200

    - WIP: HTTPClient - Moved Info.list

---
+++ Source/Network/HTTPClient.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+public class HTTPClient {
+
+    private let urlSession: URLSession
+
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+
+    @discardableResult
+    func request(with request: URLRequest) -> URLSessionDataTask? {
+        let dataTask: URLSessionDataTask = self.urlSession.dataTask(with: request) { data, response, error in
+            
+        }
+
+        dataTask.resume()
+        return dataTask
+    }
+}
